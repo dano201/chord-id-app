36eea4a65d5251ec6653995be498e3df785c5d0c
Dan <2479176B@student.gla.ac.uk>
2025-02-17 22:30:27 +0000

    Initial commit; skeleton code for back & front end

---
+++ back/utils/preprocess.py
@@ -0,0 +1,17 @@
+import librosa
+import numpy as np
+
+def process(file):
+    y, sr = librosa.load(file, sr=16000)
+
+    onset = librosa.onset.onset_detect(y=y, sr=sr, backtrack=True)
+    
+    start = 0 if len(onset) == 0 else max(onset[0] * 512 - int(sr * 0.5), 0)
+    y = y[start:]
+
+    cqt = librosa.cqt(y, sr=sr, fmin=librosa.note_to_hz('E2'), n_bins=74, bins_per_octave=24)
+    cqt_db = librosa.amplitude_to_db(np.abs(cqt), ref=np.max)
+
+    cqt_db = (cqt_db - np.min(cqt_db)) / (np.max(cqt_db) - np.min(cqt_db))
+
+    return cqt_db
